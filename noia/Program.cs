dbdc87ad183f58842d4c193d37e02b9d69267861
Gaming <Gaming@DESKTOP-J902O8N>
2025-02-15 02:45:34 +0100

    Add project files.

---
+++ noia/Program.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using System;
+
+namespace noia
+{
+    class Program
+    {
+        // Einstiegspunkt der Anwendung.
+        public static void Main(string[] args) =>
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp() =>
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+    }
+}
