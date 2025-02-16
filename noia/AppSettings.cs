c087d925d32b0b41d30016abc2af66ab305615ee
MyNameIsTobi <tobiaswoelke@me.com>
2025-02-16 13:49:45 +0100

    Added Settings an Main View

---
+++ noia/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noia
+{
+    public class AppSettings
+    {
+        public int WindowX { get; set; } = 100;  // default X position
+        public int WindowY { get; set; } = 100;  // default Y position
+
+        // Add other settings as needed (e.g., volume, theme, etc.)
+    }
+}
--- noia/MainWindow.axaml.cs
+++ noia/MainWindow.axaml.cs
@@ -1,18 +1,40 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using noia.Views;
 
 namespace noia
 {
     public partial class MainWindow : Window
     {
+        private Button _homeButton;
+        private Button _skillsButton;
+        private Button _settingsButton;
+
         public MainWindow()
         {
+            SettingsManager.LoadSettings();
             InitializeComponent();
-#if DEBUG
-            this.AttachDevTools();
-#endif
+
+            this.Opened += (sender, e) =>
+            {
+                this.Position = new PixelPoint(SettingsManager.Settings.WindowX, SettingsManager.Settings.WindowY);
+            };
+
+            this.Closing += (sender, e) =>
+            {
+                var pos = this.Position;
+                SettingsManager.Settings.WindowX = pos.X;
+                SettingsManager.Settings.WindowY = pos.Y;
+                SettingsManager.SaveSettings();
+            };
+
+            Avalonia.Threading.Dispatcher.UIThread.Post(() =>
+            {
+                var mainContent = this.FindControl<ContentControl>("MainContent");
+                mainContent.Content = new HomeView();
+            });
         }
 
         private void InitializeComponent()
@@ -20,12 +42,23 @@ namespace noia
             AvaloniaXamlLoader.Load(this);
         }
 
-        private void SettingsButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private void MinimizeButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void HomeButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             var mainContent = this.FindControl<ContentControl>("MainContent");
             if (mainContent != null)
             {
-                mainContent.Content = new SettingsView();
+                mainContent.Content = new HomeView();
             }
             else
             {
@@ -46,18 +79,17 @@ namespace noia
             }
         }
 
-        private void HomeButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private void SettingsButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             var mainContent = this.FindControl<ContentControl>("MainContent");
             if (mainContent != null)
             {
-                mainContent.Content = new HomeView();
+                mainContent.Content = new SettingsView();
             }
             else
             {
                 // Optionally log or handle the error if the control isn't found.
             }
         }
-
     }
 }
